1e34255dda0a31d9b05cb038c1f7498cf2b2e288
Luiz Felipe Apolinário <lfelipeapo@hotmail.com>
2024-11-23 21:11:59 -0300

    chore: configs

--- data/wp-content/advanced-cache.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Boost Cache Plugin - v0.0.3
-if ( ! file_exists( '/var/www/html/wp-content/plugins/jetpack-boost/app/modules/optimizations/page-cache/pre-wordpress/Boost_Cache.php' ) ) {
-return;
-}
-require_once( '/var/www/html/wp-content/plugins/jetpack-boost/app/modules/optimizations/page-cache/pre-wordpress/Boost_Cache.php');
-$boost_cache = new Automattic\Jetpack_Boost\Modules\Optimizations\Page_Cache\Pre_WordPress\Boost_Cache();
-$boost_cache->init_actions();
-$boost_cache->serve();
